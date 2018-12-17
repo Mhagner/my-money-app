eaf2b3675244f09c4bbef9070e3bb424ba4b5c2a
Mahilson Hagnner de Sousa Pereira <mahilson.pereira@lggo.com.br>
2018-12-17 14:00:22 -0200

    Ajustes parciais no componente de autenticação

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -4,10 +4,11 @@ import { Router, Route, Redirect, hashHistory } from 'react-router'
 import Dashboard from '../pages/dashboard/dashboard'
 import BillingCycle from '../pages/billingCycle/billingCycles'
 import Group from '../pages/group/group'
+import AuthOrApp from './authOrApp'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
+        <Route path='/' component={AuthOrApp} />
         <Route path='/billingCycles' component={BillingCycle}/>
         <Route path='/groups' component={Group} />
         <Redirect from='*' to='/' />
